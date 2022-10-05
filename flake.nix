181ae1f1f00d4add733e91ddc6200781d1ab7f3e
Sridhar Ratnakumar <srid@srid.ca>
2022-10-05 16:32:33 -0400

    a drv

--- flake.nix
+++ flake.nix
@@ -2,19 +2,22 @@
   inputs = {
     parsec.url = "github:nprindle/nix-parsec";
   };
-  outputs = { self, nixpkgs, parsec }: {
+  outputs = inputs@{ self, nixpkgs, ... }: {
     herculesCI.ciSystems = [ "x86_64-linux" ];
     packages =
       let
-        inherit (parsec.lib) parsec;
+        inherit (inputs.parsec.lib) parsec;
         parser = import ./parser.nix {
           inherit parsec;
           l = nixpkgs.lib;
         };
-        package = parser [./.];
+        result = parser [ ./src ];
+        mkPackage = pkgs: pkgs.runCommand "pkg" {} ''
+          echo ${builtins.trace result.Main.depends "done"} > $out
+        '';
       in {
-        aarch64-darwin.default = package;
-        x86_64-linux.default = package;
+        aarch64-darwin.default = mkPackage nixpkgs.legacyPackages.aarch64-darwin.pkgs;
+        x86_64-linux.default = mkPackage nixpkgs.legacyPackages.x86_64-linux.pkgs;
       };
   };
 }
