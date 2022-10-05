3527dbccbe837890433077347692f79ff4a94895
Sridhar Ratnakumar <srid@srid.ca>
2022-10-05 16:17:44 -0400

    ping 2

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   };
   outputs = { self, nixpkgs, parsec }: {
     herculesCI.ciSystems = [ "x86_64-linux" ];
-
     packages.aarch64-darwin.default =
       let
         inherit (parsec.lib) parsec;
